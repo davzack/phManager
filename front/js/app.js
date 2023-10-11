61f185245b92319b56ce66280efa4fdbdd68c4f3
DavZack <davsito2@gmail.com>
2023-10-11 15:46:28 -0500

    Task #3 Diseño menu usuarios, se agregan @media, menu responsive

--- front/js/app.js
+++ front/js/app.js
@@ -2,3 +2,29 @@ const toggler = document.querySelector(".btn");
 toggler.addEventListener("click",function(){
     document.querySelector("#sidebar").classList.toggle("collapsed");
 });
+
+
+$(document).ready(function() {
+    const mediaQuery = window.matchMedia("(max-width: 1000px)");
+  
+    const elemento = document.querySelector("#sidebar");
+  
+    if (mediaQuery.matches) {
+        elemento.classList.add("collapsed");
+      } else {
+        elemento.classList.remove("collapsed");
+    }
+}
+);
+
+window.addEventListener("resize", () => {
+    const mediaQuery = window.matchMedia("(max-width: 1000px)");
+  
+    const elemento = document.querySelector("#sidebar");
+  
+    if (mediaQuery.matches) {
+      elemento.classList.add("collapsed");
+    } else {
+      elemento.classList.remove("collapsed");
+    }
+  }); 
